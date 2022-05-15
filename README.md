e82b7c24be932766a68cb46eb277e0975090164a
Owen <tamia123123@naver.com>
2022-05-15 19:10:31 +0900

    update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Coin_tracker
 - coin_api를 사용하여 존재하는 코인의 정보를 볼 수 있음 
-- react 와 typescript를 사용하여 만듦
-- api fetch를 react-query hook을 사용하여 만듦
+- react 와 typescript를 사용하여 만들어보면서 typescript를 사용하는 이유와 장점을 느낄 수 있었습니다.
+- api fetch를 react-query hook을 사용하여 만들어 봄으로써 react-query의 장점을 알 수 있었습니다.
 - css는 styled-component를 활용하여 만듦
+- ApexCharts 라이브러리를 활용하여 코인의 등락 차트를 만듦
 - 중첩 라우팅을 활용하여 만듦
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,73 @@
-function Chart() {
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+import Apexcharts from "react-apexcharts"
+interface ChartProps {
+    coinId:string;
+}
+interface IHistorical {
+    time_open: string
+    time_close: string
+    open: number
+    high:number
+    low: number
+    close: number
+    volume:number
+    market_cap: number
+}
+function Chart({coinId} :ChartProps) {
+    const params = useParams();
+    const {isLoading , data} = useQuery<IHistorical[]>(['ohlcv',coinId] , ()=> fetchCoinHistory(coinId));
     return (
-        <h1>Chart</h1>
+        <div>
+            {isLoading ? ("Loading chart...") :(
+            <Apexcharts 
+                type="line" 
+                series={
+                [
+                    {   
+                        name:"price",
+                        data: data?.map((price)=>price.close) as number[]
+                    }
+
+                ]}
+                options={{
+                    theme: {
+                        mode : "dark"
+                    },
+                    chart : {
+                        height:500, 
+                        width:500,
+                        toolbar: {
+                            show:false,
+                        },
+                        background :"transparent",
+                    },
+                    stroke: {
+                        curve:"smooth",
+                        width:5,
+                    },
+                    grid: {
+                        show:false,
+                    },
+                    yaxis:{
+                        show:false,
+                    },
+                    xaxis:{
+                        labels: {
+                            show:false,
+                        },
+                        axisTicks : {
+                            show:false,
+                        },
+                        axisBorder: {
+                            show:false,
+                        }
+                    }
+                }}>
+            </Apexcharts>
+        )}
+        </div>
     )
 }
 export default Chart
